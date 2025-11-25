2f8bb8d2abc1a357ac319d62ec0dee8f060a8fc5
yonisirote <yonisirote@gmail.com>
2025-11-25 05:10:49 +0200

    tf-idf

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python3
 import argparse
 
-from lib.keyword_search import search_command, build_command
-
+from lib.keyword_search import  search_command, build_command, tf_command, idf_command, tfidf_command
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
+    subparsers.add_parser("build", help="Build and save the inverted index")
+    
+    tf_parser = subparsers.add_parser("tf", help="Get term frequency in document")
+    tf_parser.add_argument("document_id", type=int, help="Document ID")
+    tf_parser.add_argument("term", type=str, help="Term to search for")
+    
+    idf_parser = subparsers.add_parser("idf", help="Get inverse document frequency")
+    idf_parser.add_argument("term", type=str, help="Term to search for")
+    
+    tfidf_parser = subparsers.add_parser("tfidf", help="Get TF-IDF score")
+    tfidf_parser.add_argument("document_id", type=int, help="Document ID")
+    tfidf_parser.add_argument("term", type=str, help="Term to search for")
+    
     search_parser = subparsers.add_parser("search", help="Search movies using BM25")
     search_parser.add_argument("query", type=str, help="Search query")
-
-    subparsers.add_parser("build", help="Build and save the inverted index")
 
     args = parser.parse_args()
 
@@ -25,7 +35,18 @@ def main() -> None:
             print("Building inverted index...")
             build_command()
             print("Index built and saved successfully!")
-            
+        case "tf":
+            print("Getting term frequency...")
+            tf = tf_command(args.document_id, args.term)
+            print(f"Term frequency: {tf}")
+        case "idf":
+            print("Getting inverse document frequency...")
+            idf = idf_command(args.term)
+            print(f"Inverse document frequency of '{args.term}': {idf:.2f}")
+        case "tfidf":
+            print("Getting TF-IDF score...")
+            tfidf = tfidf_command(args.document_id, args.term)
+            print(f"TF-IDF score: {tfidf:.2f}")
         case _:
             parser.print_help()
 
--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,7 +1,8 @@
+import math
 import os
 import pickle
 import string
-from collections import defaultdict
+from collections import Counter, defaultdict
 
 from nltk.stem import PorterStemmer
 
@@ -9,23 +10,33 @@ from .search_utils import DEFAULT_SEARCH_LIMIT, load_movies, load_stopwords
 
 CACHE_DIR = os.path.join(os.path.dirname(__file__), "..", "..", "cache")
 
-
 class InvertedIndex:
     def __init__(self):
         self.index = defaultdict(set)
         self.docmap: dict[int, dict] = {}
+        self.term_frequencies: dict[int, Counter] = {}
 
     def __add_document(self, doc_id, text):
         cleaned_text = clean_string(text)
         tokens = tokenize(cleaned_text)
+        self.term_frequencies[doc_id] = Counter(tokens)
         for token in tokens:
             self.index[token].add(doc_id)
-
+            
     def get_documents(self, term):
-        term = term.lower()
-        if term in self.index:
-            return sorted(list(self.index[term]))
+        token = tokenize(term)
+        if len(token) > 1:
+            raise ValueError("Term must be a single word")
+        if token[0] in self.index:
+            return sorted(list(self.index[token[0]]))
         return []
+    
+    def get_tf(self, doc_id, term):
+        token = tokenize(term)
+        if len(token) > 1:
+            raise ValueError("Term must be a single word")
+        return self.term_frequencies[doc_id][token[0]]
+        
 
     def build(self):
         movies = load_movies()
@@ -40,10 +51,13 @@ class InvertedIndex:
             pickle.dump(self.index, f)
         with open(os.path.join(CACHE_DIR, "docmap.pkl"), "wb") as f:
             pickle.dump(self.docmap, f)
+        with open(os.path.join(CACHE_DIR, "term_frequencies.pkl"), "wb") as f:
+            pickle.dump(self.term_frequencies, f)
 
     def load(self):
         index_path = os.path.join(CACHE_DIR, "index.pkl")
         docmap_path = os.path.join(CACHE_DIR, "docmap.pkl")
+        term_frequencies_path = os.path.join(CACHE_DIR, "term_frequencies.pkl")
         if not os.path.exists(index_path):
             raise FileNotFoundError(f"Cache file not found: {index_path}")
         if not os.path.exists(docmap_path):
@@ -53,6 +67,8 @@ class InvertedIndex:
             self.index = pickle.load(f)
         with open(docmap_path, "rb") as f:
             self.docmap = pickle.load(f)
+        with open(term_frequencies_path, "rb") as f:
+            self.term_frequencies = pickle.load(f)
 
 
 def build_command():
@@ -60,18 +76,34 @@ def build_command():
     inverted_index.build()
     inverted_index.save()
 
+def tf_command(doc_id, term):
+    idx = InvertedIndex()
+    idx.load()
+    return idx.get_tf(doc_id, term)
 
+def idf_command(term):
+    idx = InvertedIndex()
+    idx.load()
+    doc_count = len(idx.docmap)
+    term_doc_count = len(idx.get_documents(term))
+    return math.log((doc_count + 1) / (term_doc_count + 1))
+    
+def tfidf_command(doc_id, term):
+    rf = tf_command(doc_id, term)
+    idf = idf_command(term)
+    return rf * idf
+
+    
 def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     idx = InvertedIndex()
     idx.load()
     results = []
-    cleaned_query = clean_string(query)
-    query_tokens = tokenize(cleaned_query)
+    query_tokens = tokenize(query)
     for token in query_tokens:
-            for doc_id in idx.get_documents(token):
-                results.append(idx.docmap[doc_id])
-                if len(results) >= limit:
-                        return results
+        for doc_id in idx.get_documents(token):
+            results.append(idx.docmap[doc_id])
+            if len(results) >= limit:
+                return results
     return results
 
 
@@ -84,5 +116,5 @@ def tokenize(text: str) -> list[str]:
     stop_words = load_stopwords()
     stemmer = PorterStemmer()
     return [
-        stemmer.stem(token) for token in tokens if token and token not in stop_words
+        stemmer.stem(clean_string(token)) for token in tokens if token and token not in stop_words
     ]
